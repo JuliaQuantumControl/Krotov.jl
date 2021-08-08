b0fbce8814176e9186db00c99386a53b5ef488e5
Michael Goerz <goerz@stanford.edu>
2021-08-07 23:04:44 -0400

    Link to example notebooks

--- examples/simple_state_to_state.jl
+++ examples/simple_state_to_state.jl
@@ -1,5 +1,9 @@
 # # Optimization of a State-to-State Transfer in a Two-Level-System
 
+#md # !!! tip
+#md #     This example is also available as a Jupyter notebook:
+#md #     [`simple_state_to_state.ipynb`](@__NBVIEWER_ROOT_URL__/examples/simple_state_to_state.ipynb)
+
 # This first example illustrates the basic use of the `Krotov.jl` by solving a
 # simple canonical optimization problem: the transfer of population in a two
 # level system.
--- examples/state_to_state_rwa.jl
+++ examples/state_to_state_rwa.jl
@@ -1,4 +1,8 @@
-# Optimization of a State-to-State Transfer in a Lambda System in the RWA
+# # Optimization of a State-to-State Transfer in a Lambda System in the RWA
+
+#md # !!! tip
+#md #     This example is also available as a Jupyter notebook:
+#md #     [`state_to_state_rwa.ipynb`](@__NBVIEWER_ROOT_URL__/examples/state_to_state_rwa.ipynb)
 
 # This example is illustrates the use of complex-valued control fields. This is
 # accomplished by rewriting the Hamiltonian as the sum of two independent
