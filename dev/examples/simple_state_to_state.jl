07378b0e8247466c92b4c1916e850c2e9e24509e
Documenter.jl <documenter@juliadocs.github.io>
2021-08-22 21:50:27 +0000

    build based on e98f7ed

--- dev/examples/simple_state_to_state.jl
+++ dev/examples/simple_state_to_state.jl
@@ -1,3 +1,4 @@
+using QuantumPropagators
 using QuantumControlBase
 using Krotov
 
@@ -9,7 +10,7 @@ function hamiltonian(Ω=1.0, E0=0.2)
     Ĥ₀ = -0.5 * Ω * σ̂_z
     Ĥ₁ = σ̂_x
     ϵ(t) = E0 * flattop(t, T=5, t_rise=0.3, func=:blackman)
-    return [Ĥ₀, [Ĥ₁, ϵ]]
+    return (Ĥ₀, (Ĥ₁, ϵ))
 end
 ;
 
@@ -31,8 +32,6 @@ end
 plot_pulse(ϵ::T, tlist) where T<:Function =
     plot_pulse([ϵ(t) for t in tlist], tlist)
 
-plot_pulse(H[2][2], tlist)
-
 function ket(label)
     result = Dict(
         "0" => Vector{ComplexF64}([1, 0]),
@@ -45,20 +44,35 @@ end
 using LinearAlgebra
 @test dot(ket(0), ket(1)) ≈ 0
 
-objectives = [Objective(ket(0), H, ket(1))]
+objectives = [Objective(initial_state=ket(0), generator=H, target=ket(1))]
 
 @test length(objectives) == 1
 
 problem = ControlProblem(
-    objectives,
-    Dict(
+    objectives=objectives,
+    pulse_options=Dict(
         H[2][2]  => Dict(
             :lambda_a => 5,
             :update_shape => t -> flattop(t, T=5, t_rise=0.3, func=:blackman),
         )
     ),
-    tlist
+    tlist=tlist,
 );
+
+guess_dynamics = propagate(
+        objectives[1], problem.tlist;
+        storage=true, observables=(Ψ->abs.(Ψ).^2, )
+)
+
+function plot_population(pop0::Vector, pop1::Vector, tlist)
+    fig, ax = matplotlib.pyplot.subplots(figsize=(6, 3))
+    ax.plot(tlist, pop0, label="0")
+    ax.plot(tlist, pop1, label="1")
+    ax.legend()
+    ax.set_xlabel("time")
+    ax.set_ylabel("population")
+    return fig
+end
 
 # This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
 
