1eed18ae3eca72230531028427062e303ccd4f98
Michael Goerz <goerz@stanford.edu>
2021-08-26 06:26:26 -0400

    Remove test_prop

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,14 +6,6 @@ include("generate_example_tests.jl")
 # Note: comment outer @testset to stop after first @safetestset failure
 @time @testset verbose=true "Krotov.jl Package" begin
 
-    # Note: this test is copied from QuantumPropagators.jl. It works with the
-    # master version of QuantumPropagators.jl, but not with v0.0.1. This test
-    # is only here in order to detect whether we're successfully testing
-    # against the master version of QuantumPropagators.jl. It can be removed
-    # once that has been worked out.
-    print("\n* Propagation (test_prop.jl):")
-    @time @safetestset "Propagation" begin include("test_prop.jl") end
-
     print("\n* Example 1 (examples/simple_state_to_state.jl):")
     @time @safetestset "Example 1" begin include(joinpath("examples", "simple_state_to_state.jl")) end
 
